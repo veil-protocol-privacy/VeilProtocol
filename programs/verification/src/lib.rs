6003ca937c0ce7c106048be5ee72be3c33974114
Trinity <trinity6569@gmail.com>
2025-04-14 16:54:14 +0700

    Use sp1-solana verification contract

--- programs/verification/src/lib.rs
+++ programs/verification/src/lib.rs
@@ -1,120 +1,61 @@
-use solana_program::{
-    account_info::{next_account_info, AccountInfo}, address_lookup_table::instruction, entrypoint::ProgramResult, msg, program::invoke, program_error::ProgramError, pubkey::Pubkey, system_instruction, sysvar::{rent::Rent, Sysvar}
-};
-use solana_program::entrypoint;
 use borsh::{BorshDeserialize, BorshSerialize};
-use groth16_solana::groth16::{Groth16Verifier};
-use ark_ff::bytes::{FromBytes, ToBytes};
-use std::ops::Neg;
+use solana_program::{
+    account_info::AccountInfo, entrypoint::ProgramResult, msg, program_error::ProgramError,
+    pubkey::Pubkey,
+};
 
-pub mod verifying_key;
-use verifying_key::VERIFYINGKEY;
+pub mod verify_proof;
+use verify_proof::verify_proof;
+pub mod utils;
 
-entrypoint!(process_instruction);
+#[cfg(not(feature = "no-entrypoint"))]
+solana_program::entrypoint!(process_instruction);
+
+#[cfg(not(doctest))]
+/// Derived as follows:
+///
+/// ```
+/// let client = sp1_sdk::ProverClient::new();
+/// let (pk, vk) = client.setup(YOUR_ELF_HERE);
+/// let vkey_hash = vk.bytes32();
+/// ```
+const MULTIPLIER_VKEY_HASH: &str =
+    "0x005a77634e3ec4947608d73a65ec44d13aea2147b409f14361f4de5ed1094f9b";
+
+/// The instruction data for the program.
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct SP1Groth16Proof {
+    pub proof: Vec<u8>,
+    pub sp1_public_inputs: Vec<u8>,
+}
 
 pub fn process_instruction(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    let instruction = Instruction::unpack(instruction_data)?;
-    match instruction {
-        Instruction::Verify {data} => {
-            let proof = Proof::try_from_slice(&data)?;
+    // Deserialize the SP1Groth16Proof from the instruction data.
+    let groth16_proof = SP1Groth16Proof::try_from_slice(instruction_data)
+        .map_err(|_| ProgramError::InvalidInstructionData)?;
 
-            let mut proof_a = [0u8; 64];
-            proof_a[..64].copy_from_slice(&proof.proof_a[..64]);
+    // Get the SP1 Groth16 verification key from the `sp1-solana` crate.
+    let vk = crate::verify_proof::GROTH16_VK_4_0_0_RC3_BYTES;
 
-            let mut proof_b = [0u8; 128];
-            proof_b[..128].copy_from_slice(&proof.proof_b[..128]);
-            
-            let mut proof_c = [0u8; 64];
-            proof_c[..64].copy_from_slice(&proof.proof_c[..64]);
+    // Verify the proof.
+    verify_proof(
+        &groth16_proof.proof,
+        &groth16_proof.sp1_public_inputs,
+        &MULTIPLIER_VKEY_HASH,
+        vk,
+    )
+    .map_err(|_| ProgramError::InvalidInstructionData)?;
 
-            let mut public_signals = [0u8; 32];
-            if proof.inputs.len() < 32 {
-                // fill from the last index
-                let start = 32 - proof.inputs.len();
-                public_signals[start..].copy_from_slice(&proof.inputs[..]);
-            } else {
-                public_signals[..32].copy_from_slice(&proof.inputs[..32]);
-            };
+    // Print out the public values.
+    let mut reader = groth16_proof.sp1_public_inputs.as_slice();
+    let a = u32::deserialize(&mut reader).unwrap();
+    let b = u32::deserialize(&mut reader).unwrap();
+    let c = u32::deserialize(&mut reader).unwrap();
+    msg!("Public values: (a: {}, b: {}, c: {})", a, b, c);
 
-            let result = proof_verifier(proof_a, proof_b, proof_c, public_signals);
-            msg!("result: {:?}", result);
-        }
-    }
     Ok(())
 }
-
-// Instructions that our program can execute
-#[derive(BorshSerialize, BorshDeserialize, Debug)]
-pub enum Instruction {
-    Verify { data: Vec<u8> }
-}
-
-#[derive(BorshSerialize, BorshDeserialize, Debug)]
-pub struct Proof {
-    pub inputs: Vec<u8>,
-    pub proof_a: Vec<u8>,
-    pub proof_b: Vec<u8>,
-    pub proof_c: Vec<u8>
-}
-
-
-impl Instruction {
-    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
-        let (&variant, rest) = input
-            .split_first()
-            .ok_or(ProgramError::InvalidInstructionData)?;
-
-        // Match instruction type and parse the remaining bytes based on the variant
-        match variant {
-            0 => {  
-                Ok(Self::Verify {data: rest.to_vec() })
-            }
-            _ => Err(ProgramError::InvalidInstructionData),
-        }
-    }
-}
-
- /// Verifies a Goth16 zero knowledge proof over the bn254 curve.
- fn proof_verifier(
-    mut proof_a: [u8; 64],
-    proof_b: [u8; 128],
-    proof_c: [u8; 64],
-    public_signals: [u8; 32]
-) -> bool {
-    
-    type G1 = ark_ec::short_weierstrass_jacobian::GroupAffine<ark_bn254::g1::Parameters>;
-    fn change_endianness(bytes: &[u8]) -> Vec<u8> {
-        let mut vec = Vec::new();
-        for b in bytes.chunks(32) {
-            for byte in b.iter().rev() {
-                vec.push(*byte);
-            }
-        }
-        vec
-    }
-
-    let proof_a_neg_g1: G1 = <G1 as FromBytes>::read(
-        &*[&change_endianness(proof_a.as_slice())[..], &[0u8][..]].concat(),
-    )
-    .unwrap();
-    let mut proof_a_neg = [0u8; 65];
-    <G1 as ToBytes>::write(&proof_a_neg_g1.neg(), &mut proof_a_neg[..]).unwrap();
-    let proof_a_neg = change_endianness(&proof_a_neg[..64]).try_into().unwrap();
-    
-    let public_inputs: [[u8; 32]; 1] = [public_signals];
-    proof_a = proof_a_neg;
-    let mut verifier = Groth16Verifier::new(
-        &proof_a,
-        &proof_b,
-        &proof_c,
-        &public_inputs,
-        &VERIFYINGKEY,
-    )
-    .unwrap();
-    verifier.verify().unwrap()
-}
-
---
+++ tests/src/main.rs
@@ -0,0 +1,80 @@
+
+use sp1_sdk::{include_elf, ProverClient, SP1Stdin};
+use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_sdk::{
+    commitment_config::CommitmentConfig,
+    instruction::{AccountMeta, Instruction},
+    native_token::LAMPORTS_PER_SOL,
+    program_pack::Pack,
+    pubkey::{self, Pubkey},
+    signature::Keypair,
+    signer::Signer,
+    system_instruction::create_account,
+    transaction::Transaction,
+};
+use solana_program_test::{processor, ProgramTest};
+
+use verification::{SP1Groth16Proof, process_instruction};
+
+use borsh::{BorshDeserialize, BorshSerialize};
+
+/// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
+pub const MULTIPLIER_ELF: &[u8] =  include_bytes!("../elf/multiplier");
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // let client = RpcClient::new_with_commitment(
+    //     String::from("http://127.0.0.1:8899"),
+    //     CommitmentConfig::confirmed(),
+    // );
+    let program_id = Pubkey::new_unique();
+
+    // Setup the logger.
+    sp1_sdk::utils::setup_logger();
+
+    // Setup the prover client.
+    let client = ProverClient::from_env();
+
+    // Setup the inputs.
+    let mut stdin = SP1Stdin::new();
+    stdin.write(&3);
+    stdin.write(&4);
+
+    // Setup the program for proving.
+    let (pk, _) = client.setup(MULTIPLIER_ELF);
+
+    // Generate the proof
+    let proof = client
+        .prove(&pk, &stdin)
+        .groth16()
+        .run()
+        .expect("failed to generate proof");
+
+    println!("Successfully generated proof!");
+
+    println!("Proof: {:?}", proof.bytes());
+    println!("public values: {:?}", proof.public_values);
+    let proof = SP1Groth16Proof {
+        proof: proof.bytes().to_vec(),
+        sp1_public_inputs: proof.public_values.to_vec(),
+    };
+
+    let (banks_client, payer, recent_blockhash) = ProgramTest::new(
+        "verifier-contract",
+        program_id,
+        processor!(process_instruction),
+    )
+    .start()
+    .await;
+
+    let instruction = Instruction::new_with_borsh(
+        program_id,
+        &proof,
+        vec![AccountMeta::new(payer.pubkey(), false)],
+    );
+
+    let mut transaction = Transaction::new_with_payer(&[instruction], Some(&payer.pubkey()));
+    transaction.sign(&[&payer], recent_blockhash);
+    banks_client.process_transaction(transaction).await.unwrap();
+    Ok(())
+}
