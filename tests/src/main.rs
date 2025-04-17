77527db60bba1ebf5624b79eaef276ae8fe314b7
Trinity <trinity6569@gmail.com>
2025-04-17 10:30:06 +0700

    use rpc client for testing

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -12,7 +12,6 @@ use solana_sdk::{
     system_instruction::create_account,
     transaction::Transaction,
 };
-use solana_program_test::{processor, ProgramTest};
 
 use verification::{SP1Groth16Proof, process_instruction};
 
@@ -23,10 +22,10 @@ pub const MULTIPLIER_ELF: &[u8] =  include_bytes!("../elf/multiplier");
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // let client = RpcClient::new_with_commitment(
-    //     String::from("http://127.0.0.1:8899"),
-    //     CommitmentConfig::confirmed(),
-    // );
+    let rpc_client = RpcClient::new_with_commitment(
+        String::from("http://127.0.0.1:8899"),
+        CommitmentConfig::confirmed(),
+    );
     let program_id = Pubkey::new_unique();
 
     // Setup the logger.
@@ -52,6 +51,7 @@ async fn main() -> anyhow::Result<()> {
 
     println!("Successfully generated proof!");
 
+    proof.save("../proof.bin").expect("saving proof failed");
     println!("Proof: {:?}", proof.bytes());
     println!("public values: {:?}", proof.public_values);
     let proof = SP1Groth16Proof {
@@ -59,13 +59,17 @@ async fn main() -> anyhow::Result<()> {
         sp1_public_inputs: proof.public_values.to_vec(),
     };
 
-    let (banks_client, payer, recent_blockhash) = ProgramTest::new(
-        "verifier-contract",
-        program_id,
-        processor!(process_instruction),
-    )
-    .start()
-    .await;
+    let payer = Keypair::new();
+
+    let transaction_signature = rpc_client
+        .request_airdrop(&payer.pubkey(), 5 * LAMPORTS_PER_SOL)
+        .await?;
+    loop {
+        if rpc_client.confirm_transaction(&transaction_signature).await? {
+            break;
+        }
+    }
+
 
     let instruction = Instruction::new_with_borsh(
         program_id,
@@ -74,7 +78,10 @@ async fn main() -> anyhow::Result<()> {
     );
 
     let mut transaction = Transaction::new_with_payer(&[instruction], Some(&payer.pubkey()));
-    transaction.sign(&[&payer], recent_blockhash);
-    banks_client.process_transaction(transaction).await.unwrap();
+    transaction.sign(&[&payer], rpc_client.get_latest_blockhash().await?);
+    match rpc_client.send_and_confirm_transaction(&transaction).await {
+        Ok(signature) => println!("Transaction Signature: {}", signature),
+        Err(err) => eprintln!("Error sending transaction: {}", err),
+    }
     Ok(())
 }
